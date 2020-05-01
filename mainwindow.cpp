b211b20ba1ea542ad9f2c19d65a48d70c89644b4
seanald <sean_m_fitzgerald@tutanota.com>
2020-04-30 18:28:35 -0700

    UI Tweaks, cleanup, and setting up for implementing copying

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,13 +3,21 @@
 
 #include <iostream>
 
+using namespace std;
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    file_dialog_open = false;
+    // UI elements
+    src_list_widget = findChild<QListWidget*>("srcList");
+    dst_list_widget = findChild<QListWidget*>("dstList");
+
+    //img_label = findChild<QLabel*>("imgLabel");
+    img_scroll_area = findChild<QScrollArea*>("imgScrollArea");
+
     file_dialog = nullptr;
 }
 MainWindow::~MainWindow()
@@ -44,6 +52,10 @@ void MainWindow::on_nextButton_clicked()
 }
 void MainWindow::on_addDstButton_clicked()
 {
+    openFileDialog(QFileDialog::Directory);
+    connect(file_dialog, &QFileDialog::filesSelected,
+            this, &MainWindow::dstFilesSelected);
+    file_dialog->show();
 }
 void MainWindow::on_rmDstButton_clicked()
 {
@@ -51,19 +63,22 @@ void MainWindow::on_rmDstButton_clicked()
 void MainWindow::on_pushButton_clicked()
 {
 }
+void MainWindow::on_copyButton_clicked()
+{
+    queueCopy();
+}
 void MainWindow::srcFilesSelected(QStringList selected)
 {
     QStringList src_files = expandFileList(selected);
+    cout << "--------------------\n"
+         << "Adding source files:\n";
     for(int i=0; i < src_files.size(); i++)
     {
-        std::cout << src_files.at(i).toLocal8Bit().data() << "\n";
-//        addSrc(src_files.at(i));
-//        if(isImage(src_files.at(i)))
-//            QListWidgetItem src_file_item(QIcon(src_files.at(i)),
-//                                          src_files.at(i),
-//                                          src_list,
-//                                          QListWidgetItem::Type);
+        cout << src_files.at(i).toLocal8Bit().data() << endl;
+        addSrc(src_files.at(i));
     }
+    cout << "Done.\n"
+         << "--------------------\n";
 
 }
 void MainWindow::dstFilesSelected(QStringList selected)
@@ -75,19 +90,6 @@ void MainWindow::dstFilesSelected(QStringList selected)
             addDst(currentFile);
     }
 }
-QStringList MainWindow::expandDir(QString dir)
-{
-    QStringList new_file_list;
-    QDirIterator it(dir, QStringList() << "scaling_cur_freq",
-                    QDir::NoFilter, QDirIterator::Subdirectories);
-    while(it.hasNext())
-    {
-        QString next = it.next();
-        if(QFileInfo(next).isDir() == false && isSupportedFile(next))
-            new_file_list << next;
-    }
-    return new_file_list;
-}
 QStringList MainWindow::expandFileList(QStringList file_list)
 {
     QStringList new_file_list;
@@ -96,7 +98,16 @@ QStringList MainWindow::expandFileList(QStringList file_list)
         QString file_name = file_list.at(i);
         QFileInfo file_info(file_name);
         if(file_info.isDir())
-            new_file_list << expandDir(file_name);
+        {
+            QDirIterator it(file_name, QStringList(),
+                            QDir::NoFilter, QDirIterator::Subdirectories);
+            while(it.hasNext())
+            {
+                QString next = it.next();
+                if(QFileInfo(next).isDir() == false && isSupportedFile(next))
+                    new_file_list << next;
+            }
+        }
         else if(isSupportedFile(file_name))
              new_file_list << file_name;
     }
@@ -105,19 +116,54 @@ QStringList MainWindow::expandFileList(QStringList file_list)
 bool MainWindow::isSupportedFile(QString file)
 {
     QImageReader image_reader(file);
-    if(image_reader.canRead())
-        return true;
-    return false;
+    QImage image;
+    return image_reader.read(&image);
 }
 void MainWindow::openFileDialog(QFileDialog::FileMode mode)
 {
-    file_dialog_open = true;
     file_dialog = new QFileDialog();
     file_dialog->setFileMode(mode);
 }
 void MainWindow::addSrc(QString src)
 {
+    for(int i=0; i < src_files.size(); i++)
+        if(src_files.at(i).compare(src) == 0)
+            return;
+    src_files << src;
+    new QListWidgetItem(QIcon(src), "", src_list_widget,
+                        QListWidgetItem::Type);
 }
 void MainWindow::addDst(QString dst)
 {
+    for(int i=0; i < dst_dirs.size(); i++)
+        if(dst_dirs.at(i).compare(dst) == 0)
+            return;
+    dst_dirs << dst;
+    new QListWidgetItem(dst, dst_list_widget, QListWidgetItem::Type);
+}
+void MainWindow::on_dstList_itemDoubleClicked(QListWidgetItem* item)
+{
+    queueCopy();
+}
+void MainWindow::on_srcList_currentItemChanged(
+        QListWidgetItem* current,
+        QListWidgetItem* previous)
+{
+    int i = src_list_widget->row(current);
+    img_label = new QLabel();
+    img_label->setPixmap(QPixmap(src_files.at(i)));
+    img_scroll_area->setWidget(img_label);
+}
+void MainWindow::on_dstList_currentItemChanged(
+        QListWidgetItem* current,
+        QListWidgetItem* previous)
+{
+    int i = dst_list_widget->row(current);
+    current_dst = dst_dirs.at(i);
+}
+
+/* Queueing file operations */
+void MainWindow::queueCopy()
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -8,6 +8,16 @@
 #include <QImageReader>
 #include <QImage>
 #include <QDirIterator>
+#include <QLabel>
+#include <QPushButton>
+
+#include "ui_mainwindow.h"
+
+struct CopyOp
+{
+    QString src;
+    QString dst;
+};
 
 namespace Ui {
 class MainWindow;
@@ -24,15 +34,34 @@ public:
 private:
     Ui::MainWindow *ui;
 
-    bool file_dialog_open;
+    /* UI elements */
+    QListWidget* src_list_widget;
+    QListWidget* dst_list_widget;
+    QLabel* img_label;
+    QScrollArea* img_scroll_area;
     QFileDialog* file_dialog;
-    QListWidget* src_list = findChild<QListWidget*>("srcList");
+
+    /* File-related data fields */
+    QStringList src_files;
+    QStringList dst_dirs;
+    QString current_src;
+    QString current_dst;
+
+    /* Operation queue lists */
+    QList<QString*> queue_list_copy;
+    // A list of pointers to two-item QString arrays. Each array contains
+    // the source file to be copied and the destination directory.
+
 
     void openFileDialog(QFileDialog::FileMode mode);
     void addSrc(QString src);
     void addDst(QString dst);
+    QStringList expandFileList(QStringList file_list);
+    bool isSupportedFile(QString file);
 
-    QStringList expandDir(QString dir);
+
+    /* Queueing file operations. */
+    void queueCopy();
 
 private slots:
     void on_addDirButton_clicked();
@@ -44,12 +73,18 @@ private slots:
     void on_addDstButton_clicked();
     void on_rmDstButton_clicked();
     void on_pushButton_clicked();
+    void on_copyButton_clicked();
+    void on_srcList_currentItemChanged(
+            QListWidgetItem* current,
+            QListWidgetItem* previous);
+    void on_dstList_currentItemChanged(
+            QListWidgetItem* current,
+            QListWidgetItem* previous);
+    void on_dstList_itemDoubleClicked(QListWidgetItem* item);
 
     void srcFilesSelected(QStringList selected);
     void dstFilesSelected(QStringList selected);
 
-    QStringList expandFileList(QStringList file_list);
-    bool isSupportedFile(QString file);
 };
 
 #endif // MAINWINDOW_H
